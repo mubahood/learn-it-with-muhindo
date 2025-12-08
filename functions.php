047785115382353ff67977e04852c2a77326fb64
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-12-08 15:52:11 +0300

    Refactor database connection string in getDBConnection function; remove unix_socket parameter for improved compatibility.

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ require_once 'config.php';
 // Database connection function
 function getDBConnection() {
     try {
-        $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";unix_socket=" . DB_SOCKET;
+        $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME ;
         $pdo = new PDO($dsn, DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
